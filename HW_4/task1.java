55a03d3e7e8f8af6b8d5dcefef9708d7df9a6ee6
JennaFi <filimonova.evgeniya@gmail.com>
2023-07-14 01:33:59 +0300

    changed HW4

--- HW_4/task1.java
+++ HW_4/task1.java
@@ -5,52 +5,74 @@
 package HW_4;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Random;
 
 public class task1 {
 
     public static void main(String[] args) {
-        // multiply(0, 0);
-        sumDeque(1, 6);
+        Deque<Integer> d1 = new ArrayDeque<>(Arrays.asList(8, -2));
+        Deque<Integer> d2 = new ArrayDeque<>(Arrays.asList(8));
+        Deque<Integer> d3 = new ArrayDeque<>(Arrays.asList(5, 2));
+        Deque<Integer> d4 = new ArrayDeque<>(Arrays.asList(4));
+
+        System.out.println(multiply(d3, d4));
+
+        System.out.println(sum(d1, d2));
     }
 
-    static void multiply(int a, int b) {
-        Deque<Integer> deque = new ArrayDeque<>(2);
-        Random random = new Random();
-        a = random.nextInt(100);
-        b = random.nextInt(100);
-        deque.addFirst(a);
-        deque.addLast(b);
-        //
-        LinkedList list = new LinkedList();
-        list.add(deque.getFirst() * deque.getLast());
-
-        for (Integer i : deque) {
-            System.out.println(i);
+    public static Deque<Integer> sum(Deque<Integer> d1, Deque<Integer> d2) {
+        Deque<Integer> result = new ArrayDeque<>();
+        int count = 0;
+        while (!d1.isEmpty() || !d2.isEmpty()) {
+            int sum = count;
+            if (!d1.isEmpty()) {
+                sum += d1.removeFirst();
+            }
+            if (!d2.isEmpty()) {
+                sum += d2.removeFirst();
+            }
+            result.addLast(sum % 10);
+            count = sum / 10;
         }
-
-        System.out.print(list);
+        if (count != 0) {
+            result.addLast(count);
+        }
+        return result;
     }
 
-    static void sumDeque(int minValue, int maxValue) {
-        Deque<Integer> deque = new ArrayDeque<>(2);
-        Random random = new Random();
-        int a = random.nextInt(100);
-        int b = random.nextInt(100);
+    public static Deque<Integer> multiply(Deque<Integer> d1, Deque<Integer> d2) {
+        Deque<Integer> res = new ArrayDeque<>();
 
-        deque.addFirst(a * (-1));
-        deque.addLast(b);
+        int temp1 = 0;
+        int temp2 = 0;
+        int mult = 0;
 
-        LinkedList list = new LinkedList();
-        list.add(deque.getFirst() + deque.getLast());
+        for (int i = 0; i < d1.size() + 1; i++) {
+            mult = d1.removeFirst() * d2.getFirst() + temp1;
 
-        for (Integer i : deque) {
-            System.out.println(i);
+            if (mult > 9) {
+                temp1 = mult / 10;
+                temp2 = mult % 10;
+
+                if (temp2 > 9) {
+                    res.addLast(temp2 % 10);
+                    res.addLast(temp2 / 10);
+                }
+                res.addLast(temp2);
+
+            } else {
+                res.addLast(temp2);
+                temp1 = mult / 10;
+            }
         }
 
-        System.out.print(list);
+        if (temp1 > 0) {
+            res.addLast(temp1);
+        }
+
+        return res;
+
     }
 
 }
